87add412612317c5aa58e3e90c6802eadf1211c6
Max Liu <zl2480@columbia.edu>
2017-01-18 22:43:18 -0500

    update

--- sort_colors.py
+++ sort_colors.py
@@ -1,13 +1,21 @@
 class Solution(object):
-    
-    def sortColors(self, nums):
-        i = j = 0
-        for k in xrange(len(nums)):
-            v = nums[k]
-            nums[k] = 2
-            if v < 2:
-                nums[j] = 1
-                j += 1
-            if v == 0:
-                nums[i] = 0
-                i += 1
+    def combine(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: List[List[int]]
+        """
+        self.ret = []
+        self.dfs(n, 1, 0, k, [])
+        return self.ret
+
+    def dfs(self, n, start, count,k, value):
+        if count == k:
+            return self.ret.append(value)
+        for i in range(start, n+1):
+            self.dfs(n, i + 1, count + 1, k, value + [i])
+
+
+s = Solution()
+t = s.combine(4,2)
+print t
